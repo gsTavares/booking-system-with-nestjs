e36533f68254beaee7addf175b527169c21c3c6b
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-12-14 19:20:30 -0300

    feat: database configuration in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { Booking } from "./bookings/booking.entity";
+import { Space } from "./spaces/space.entity";
+import { User } from "./users/user.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postdba",
+      database: "booking_system",
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      entities: [User, Space, Booking],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
