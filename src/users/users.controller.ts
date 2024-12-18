b280f4c27f8701b23e0f0f2365f3d350026f2d54
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-12-18 20:26:55 -0300

    feat: listing users and role changing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { RolesGuard } from "src/auth/guards/roles.guard";
+import { UsersService } from "./users.service";
+import { Roles } from "src/auth/decorators/roles.decorator";
 
 @Controller("users")
-export class UsersController {}
+@UseGuards(AuthGuard("jwt"), RolesGuard)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @Roles("admin")
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Patch(":id/role")
+  @Roles("admin")
+  async updateRole(
+    @Request() req: { user: { userId: string } },
+    @Param("id") userId: string,
+    @Body("role") newRole: string,
+  ) {
+    if (req.user.userId === userId) {
+      throw new HttpException(
+        "Cannot change your own role",
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    if (!["admin", "user"].includes(newRole.toLowerCase())) {
+      throw new HttpException("Invalid role", HttpStatus.BAD_REQUEST);
+    }
+    const result = await this.usersService.updateRole(userId, newRole);
+    const { id, name, email, role, created_at, updated_at } = result;
+    return { id, name, email, role, created_at, updated_at };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { User } from "./user.entity";
 import { Repository } from "typeorm";
+import { User } from "./user.entity";
 
 @Injectable()
 export class UsersService {
@@ -16,6 +16,21 @@ export class UsersService {
 
   async create(userData: Partial<User>): Promise<User> {
     const user = this.userRepository.create(userData);
+    return this.userRepository.save(user);
+  }
+
+  async findAll() {
+    return this.userRepository.find({
+      select: ["id", "name", "email", "role", "created_at", "updated_at"],
+    });
+  }
+
+  async updateRole(userId: string, role: string) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException("User not found");
+    }
+    user.role = role;
     return this.userRepository.save(user);
   }
 }
