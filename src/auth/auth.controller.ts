d3033d8f053db84ae005ff0ed761583ca961eadf
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-12-18 21:04:44 -0300

    refac: rename user dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,16 +8,16 @@ import {
   UseGuards,
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import { CreateUserData } from "src/users/dtos/create-user.dto";
+import { CreateUserDto } from "src/users/dtos/create-user.dto";
 import { AuthService } from "./auth.service";
-import { LoginUserData } from "src/users/dtos/login-user.dto";
+import { LoginUserDto } from "src/users/dtos/login-user.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("login")
-  async login(@Body() body: LoginUserData) {
+  async login(@Body() body: LoginUserDto) {
     const user = await this.authService.validateUser(body.email, body.password);
     if (!user) {
       throw new UnauthorizedException("Invalid Credentials");
@@ -26,7 +26,7 @@ export class AuthController {
   }
 
   @Post("register")
-  async register(@Body() body: CreateUserData) {
+  async register(@Body() body: CreateUserDto) {
     const result = await this.authService.register(body);
     const { id, name, email, role, created_at } = result;
     return { id, name, email, role, created_at };
